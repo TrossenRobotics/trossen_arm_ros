213d1575e6a99df640067bf89f1082f7ce7b6167
Luke Schmitt <luke@trossenrobotics.com>
2025-01-21 21:35:12 +0000

    Add arm system interface

---
+++ trossen_arm_hardware/include/trossen_arm_hardware/interface.hpp
@@ -0,0 +1,125 @@
+// Copyright 2025 Trossen Robotics
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//    * Redistributions of source code must retain the above copyright
+//      notice, this list of conditions and the following disclaimer.
+//
+//    * Redistributions in binary form must reproduce the above copyright
+//      notice, this list of conditions and the following disclaimer in the
+//      documentation and/or other materials provided with the distribution.
+//
+//    * Neither the name of the the copyright holder nor the names of its
+//      contributors may be used to endorse or promote products derived from
+//      this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+#ifndef TROSSEN_ARM_HARDWARE__INTERFACE_HPP_
+#define TROSSEN_ARM_HARDWARE__INTERFACE_HPP_
+
+#include <memory>
+
+#include "hardware_interface/hardware_info.hpp"
+#include "hardware_interface/system_interface.hpp"
+#include "hardware_interface/types/hardware_interface_return_values.hpp"
+#include "hardware_interface/types/hardware_interface_type_values.hpp"
+#include "hardware_interface/types/lifecycle_state_names.hpp"
+#include "hardware_interface/visibility_control.h"
+#include "rclcpp_lifecycle/state.hpp"
+#include "rclcpp/logger.hpp"
+#include "rclcpp/rclcpp.hpp"
+
+#include "trossen_arm_driver/trossen_arm_driver.hpp"
+
+namespace trossen_arm_hardware
+{
+
+static const std::string DRIVER_IP_ADDRESS_DEFAULT = "192.168.1.2";
+static const uint16_t DRIVER_PORT_DEFAULT = 50000;
+static const uint32_t DRIVER_TIMEOUT_US_DEFAULT = 2000;
+
+using CallbackReturn = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
+
+using hardware_interface::CommandInterface;
+using hardware_interface::HardwareInfo;
+using hardware_interface::HW_IF_EFFORT;
+using hardware_interface::HW_IF_POSITION;
+using hardware_interface::HW_IF_VELOCITY;
+using hardware_interface::return_type;
+using hardware_interface::StateInterface;
+using trossen_arm::TrossenArmDriver;
+
+class TrossenArmHardwareInterface : public hardware_interface::SystemInterface
+{
+public:
+  RCLCPP_SHARED_PTR_DEFINITIONS(TrossenArmHardwareInterface)
+
+  CallbackReturn on_init(const HardwareInfo & info) override;
+
+  std::vector<hardware_interface::StateInterface> export_state_interfaces() override;
+
+  std::vector<hardware_interface::CommandInterface> export_command_interfaces() override;
+
+  CallbackReturn on_configure(const rclcpp_lifecycle::State & previous_state) override;
+
+  CallbackReturn on_activate(const rclcpp_lifecycle::State & previous_state) override;
+
+  CallbackReturn on_deactivate(const rclcpp_lifecycle::State & previous_state) override;
+
+  CallbackReturn on_cleanup(const rclcpp_lifecycle::State & previous_state) override;
+
+  return_type read(const rclcpp::Time & time, const rclcpp::Duration & duration) override;
+
+  return_type write(const rclcpp::Time & time, const rclcpp::Duration & duration) override;
+
+protected:
+  // The driver for the robot
+  std::unique_ptr<TrossenArmDriver> arm_driver_{nullptr};
+
+  trossen_arm::Model robot_model_;
+
+  std::string driver_ip_address_{DRIVER_IP_ADDRESS_DEFAULT};
+
+  uint16_t driver_port_{DRIVER_PORT_DEFAULT};
+
+  uint32_t driver_timeout_us_{DRIVER_TIMEOUT_US_DEFAULT};
+
+  // Joint positions in radians
+  std::vector<double> joint_positions_;
+
+  // Joint velocities in radians per second
+  std::vector<double> joint_velocities_;
+
+  // Joint efforts in Newton-meters
+  std::vector<double> joint_efforts_;
+
+  // Joint position commands in radians
+  std::vector<double> joint_position_commands_;
+
+  // // Joint velocity commands in radians per second
+  // std::vector<double> joint_velocity_commands_;
+
+  // // Joint effort commands in Newton-meters
+  // std::vector<double> joint_effort_commands_;
+
+  // Logger
+  static rclcpp::Logger get_logger() {
+    return rclcpp::get_logger("trossen_arm_hardware");
+  };
+};
+
+}  // namespace trossen_arm_hardware
+
+#endif  // TROSSEN_ARM_HARDWARE__INTERFACE_HPP_
---
+++ trossen_arm_hardware/src/interface.cpp
@@ -0,0 +1,329 @@
+// Copyright 2025 Trossen Robotics
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//    * Redistributions of source code must retain the above copyright
+//      notice, this list of conditions and the following disclaimer.
+//
+//    * Redistributions in binary form must reproduce the above copyright
+//      notice, this list of conditions and the following disclaimer in the
+//      documentation and/or other materials provided with the distribution.
+//
+//    * Neither the name of the the copyright holder nor the names of its
+//      contributors may be used to endorse or promote products derived from
+//      this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+#include "trossen_arm_hardware/interface.hpp"
+
+namespace trossen_arm_hardware
+{
+
+using CallbackReturn = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
+
+CallbackReturn
+TrossenArmHardwareInterface::on_init(const hardware_interface::HardwareInfo & info)
+{
+  if (hardware_interface::SystemInterface::on_init(info) != CallbackReturn::SUCCESS) {
+    return CallbackReturn::ERROR;
+  }
+
+  // Get robot model, ip, port, timeout
+
+  try {
+    robot_model_ = trossen_arm::Model(std::stoi(info.hardware_parameters.at("robot_model")));
+  } catch (const std::out_of_range & /*e*/) {
+    RCLCPP_FATAL(
+      get_logger(),
+      "Required parameter 'robot_model' not specified.");
+    return CallbackReturn::FAILURE;
+  } catch (const std::invalid_argument & /*e*/) {
+    // TODO(lukeschmitt-tr): validate robot model
+    // RCLCPP_FATAL(
+    //   get_logger(),
+    //   "Invalid 'robot_model' value specified: '%s'.", robot_model);
+    return CallbackReturn::FAILURE;
+  }
+
+  try {
+    driver_ip_address_ = info.hardware_parameters.at("ip_address");
+  } catch (const std::out_of_range & /*e*/) {
+    RCLCPP_FATAL(
+      get_logger(),
+      "Parameter 'ip_address' not specified. Defaulting to '%s'.",
+      DRIVER_IP_ADDRESS_DEFAULT.c_str());
+    driver_ip_address_ = DRIVER_IP_ADDRESS_DEFAULT;
+  }
+
+  // TODO(lukeschmitt-tr): Log robot_model, ip_address
+
+  joint_positions_.resize(info_.joints.size(), std::numeric_limits<double>::quiet_NaN());
+  joint_velocities_.resize(info_.joints.size(), std::numeric_limits<double>::quiet_NaN());
+  joint_efforts_.resize(info_.joints.size(), std::numeric_limits<double>::quiet_NaN());
+
+  for (const auto & joint : info_.joints) {
+    // Only position command interfaces are supported for now
+    if (joint.command_interfaces.size() != 1) {
+      RCLCPP_ERROR(
+        get_logger(),
+        "Joint '%s' has %zu command interfaces found. 1 expected.",
+        joint.name.c_str(), joint.command_interfaces.size());
+      return CallbackReturn::ERROR;
+    }
+
+    if (joint.command_interfaces.at(0).name != HW_IF_POSITION) {
+      RCLCPP_ERROR(
+        get_logger(),
+        "Joint '%s' has '%s' command interface found. '%s' expected",
+        joint.name.c_str(), joint.command_interfaces.at(0).name.c_str(), HW_IF_POSITION);
+      return CallbackReturn::ERROR;
+    }
+
+    // Each joint has 3 state interfaces: position, velocity, effort (in that order)
+    if (joint.state_interfaces.size() != 3) {
+      RCLCPP_ERROR(
+        get_logger(),
+        "Joint '%s' has %zu state interfaces found. 3 expected.",
+        joint.name.c_str(), joint.state_interfaces.size());
+      return CallbackReturn::ERROR;
+    }
+
+    // Position first
+    if (joint.state_interfaces.at(0).name != HW_IF_POSITION) {
+      RCLCPP_ERROR(
+        get_logger(),
+        "Joint '%s' has '%s' state interface found. '%s' expected",
+        joint.name.c_str(), joint.state_interfaces.at(0).name.c_str(), HW_IF_POSITION);
+      return CallbackReturn::ERROR;
+    }
+
+    // Velocity second
+    if (joint.state_interfaces.at(1).name != HW_IF_VELOCITY) {
+      RCLCPP_ERROR(
+        get_logger(),
+        "Joint '%s' has '%s' state interface found. '%s' expected",
+        joint.name.c_str(), joint.state_interfaces.at(1).name.c_str(), HW_IF_VELOCITY);
+      return CallbackReturn::ERROR;
+    }
+
+    // Effort third
+    if (joint.state_interfaces.at(2).name != HW_IF_EFFORT) {
+      RCLCPP_ERROR(
+        get_logger(),
+        "Joint '%s' has '%s' state interface found. '%s' expected",
+        joint.name.c_str(), joint.state_interfaces.at(2).name.c_str(), HW_IF_EFFORT);
+      return CallbackReturn::ERROR;
+    }
+  }
+
+  return CallbackReturn::SUCCESS;
+}
+
+std::vector<hardware_interface::StateInterface>
+TrossenArmHardwareInterface::export_state_interfaces()
+{
+  std::vector<hardware_interface::StateInterface> state_interfaces;
+  for (size_t i = 0; i < info_.joints.size(); ++i) {
+    state_interfaces.emplace_back(
+      hardware_interface::StateInterface(
+        info_.joints[i].name, HW_IF_POSITION, &joint_positions_[i]));
+    state_interfaces.emplace_back(
+      hardware_interface::StateInterface(
+        info_.joints[i].name, HW_IF_VELOCITY, &joint_velocities_[i]));
+    state_interfaces.emplace_back(
+      hardware_interface::StateInterface(
+        info_.joints[i].name, HW_IF_EFFORT, &joint_efforts_[i]));
+  }
+
+  return state_interfaces;
+}
+
+std::vector<hardware_interface::CommandInterface>
+TrossenArmHardwareInterface::export_command_interfaces()
+{
+  std::vector<hardware_interface::CommandInterface> command_interfaces;
+  for (size_t i = 0; i < info_.joints.size(); i++) {
+    command_interfaces.emplace_back(
+      hardware_interface::CommandInterface(
+        info_.joints[i].name, HW_IF_POSITION, &joint_position_commands_[i]));
+  }
+  return command_interfaces;
+}
+
+CallbackReturn
+TrossenArmHardwareInterface::on_configure(const rclcpp_lifecycle::State & /*previous_state*/)
+{
+  try {
+    arm_driver_ = std::make_unique<TrossenArmDriver>();
+  } catch (const std::exception & e) {
+    RCLCPP_FATAL(
+      get_logger(),
+      "Failed to create TrossenArmDriver: %s", e.what());
+    return CallbackReturn::ERROR;
+  }
+
+  if (!arm_driver_) {
+    RCLCPP_FATAL(
+      get_logger(),
+      "Failed to create TrossenArmDriver.");
+    return CallbackReturn::ERROR;
+  }
+
+  if (!arm_driver_->configure(
+    robot_model_,
+    driver_ip_address_.c_str(),
+    driver_port_,
+    driver_timeout_us_))
+  {
+    RCLCPP_FATAL(
+      get_logger(),
+      "Failed to configure TrossenArmDriver.");
+    return CallbackReturn::ERROR;
+  }
+
+  RCLCPP_INFO(
+    get_logger(),
+    "TrossenArmDriver configured with model %d, ip %s, port %d, timeout %d",
+    static_cast<int>(robot_model_), driver_ip_address_.c_str(), driver_port_, driver_timeout_us_);
+
+  return CallbackReturn::SUCCESS;
+}
+
+CallbackReturn
+TrossenArmHardwareInterface::on_activate(const rclcpp_lifecycle::State & /*previous_state*/)
+{
+  for (auto & position : joint_position_commands_) {
+    if (std::isnan(position)) {
+      position = 0.0;
+    }
+  }
+
+  // Enable the arm
+  if (!arm_driver_->enable()) {
+    RCLCPP_FATAL(
+      get_logger(),
+      "Failed to enable TrossenArmDriver.");
+    return CallbackReturn::ERROR;
+  }
+
+  // Sanity check that the arm is enabled
+  if (!arm_driver_->get_enabled()) {
+    RCLCPP_FATAL(
+      get_logger(),
+      "TrossenArmDriver failed to enable.");
+    return CallbackReturn::ERROR;
+  }
+
+  RCLCPP_INFO(get_logger(), "TrossenArmDriver enabled.");
+
+  return CallbackReturn::SUCCESS;
+}
+
+return_type
+TrossenArmHardwareInterface::read(
+  const rclcpp::Time & /*time*/,
+  const rclcpp::Duration & /*period*/)
+{
+  auto positions = arm_driver_->get_positions();
+  joint_positions_ = std::vector<double>(positions.begin(), positions.end());
+
+  auto velocities = arm_driver_->get_velocities();
+  joint_velocities_ = std::vector<double>(velocities.begin(), velocities.end());
+
+  auto efforts = arm_driver_->get_external_torques();
+  joint_efforts_ = std::vector<double>(efforts.begin(), efforts.end());
+
+  return return_type::OK;
+}
+
+return_type
+TrossenArmHardwareInterface::write(
+  const rclcpp::Time & /*time*/,
+  const rclcpp::Duration & /*period*/)
+{
+  // Check that all arm joints are in the correct mode
+  auto modes = arm_driver_->get_modes();
+  if (modes.size() - 1 != joint_position_commands_.size()) {
+    RCLCPP_ERROR(
+      get_logger(),
+      "Number of joint modes does not match number of joint position commands.");
+    return return_type::ERROR;
+  }
+
+  if (std::any_of(
+      modes.begin(), modes.end(),
+      [](trossen_arm::Mode mode) {return mode != trossen_arm::Mode::POSITION;}))
+  {
+    RCLCPP_ERROR(
+      get_logger(),
+      "Joint modes are not all POSITION.");
+    return return_type::ERROR;
+  }
+
+  auto position_commands = std::vector<float>(
+    joint_position_commands_.begin(), joint_position_commands_.end());
+
+  // Validate that we are not sending NaN or INF values to the driver
+  if (std::any_of(
+      position_commands.begin(), position_commands.end(),
+      [](float position) {return (std::isnan(position) || std::isinf(position));}))
+  {
+    RCLCPP_ERROR(
+      get_logger(),
+      "Position command contains NaN or INF values.");
+    return return_type::ERROR;
+  }
+
+  if (!arm_driver_->set_positions(position_commands)) {
+    RCLCPP_ERROR(
+      get_logger(),
+      "Failed to set joint positions.");
+    return return_type::ERROR;
+  }
+
+  return return_type::OK;
+}
+
+CallbackReturn
+TrossenArmHardwareInterface::on_deactivate(const rclcpp_lifecycle::State & /*previous_state*/)
+{
+  if (!arm_driver_->disable()) {
+    RCLCPP_FATAL(
+      get_logger(),
+      "Failed to disable TrossenArmDriver.");
+    return CallbackReturn::ERROR;
+  }
+
+  RCLCPP_INFO(
+    get_logger(),
+    "TrossenArmDriver disabled.");
+
+  return CallbackReturn::SUCCESS;
+}
+
+CallbackReturn
+TrossenArmHardwareInterface::on_cleanup(const rclcpp_lifecycle::State & /*previous_state*/)
+{
+  arm_driver_.reset();
+
+  return CallbackReturn::SUCCESS;
+}
+
+}  // namespace trossen_arm_hardware
+
+#include "pluginlib/class_list_macros.hpp"  // NOLINT
+PLUGINLIB_EXPORT_CLASS(
+  trossen_arm_hardware::TrossenArmHardwareInterface,
+  hardware_interface::SystemInterface)
